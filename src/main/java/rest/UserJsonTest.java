9a371eedb124735e01a6b8c66783ba1f4bb9179c
George Salu <george.salu10@gmail.com>
2019-08-25 16:16:20 -0300

    verificações avancadas

--- src/main/java/rest/UserJsonTest.java
+++ src/main/java/rest/UserJsonTest.java
@@ -113,7 +113,14 @@ public class UserJsonTest {
             .statusCode(200)
             .body("$", hasSize(3))
             .body("age.findAll{it <= 25}.size()", is(2))
-            .body("age.findAll{it <= 25 && it > 20}.size()", is(1));
+            .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+            .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+            .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+            .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+            .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+            .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"));
+
+
 
 
     }
