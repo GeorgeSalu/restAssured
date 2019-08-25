63c3864455c40f57ceaf1ae50ba68b2606e199d5
George Salu <george.salu10@gmail.com>
2019-08-25 15:50:28 -0300

    verificações avancadas

--- src/main/java/rest/UserJsonTest.java
+++ src/main/java/rest/UserJsonTest.java
@@ -103,4 +103,19 @@ public class UserJsonTest {
 
     }
 
+    @Test
+    public void devoFazerVerificacaoesAvancadas() {
+
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users")
+        .then()
+            .statusCode(200)
+            .body("$", hasSize(3))
+            .body("age.findAll{it <= 25}.size()", is(2))
+            .body("age.findAll{it <= 25 && it > 20}.size()", is(1));
+
+
+    }
+
 }
