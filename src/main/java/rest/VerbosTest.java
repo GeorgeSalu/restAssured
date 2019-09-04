1ae7d0e4096b3449193b45fc4a470cdc12457e70
George Salu <george.salu10@gmail.com>
2019-09-03 22:44:20 -0300

    inserindo com POST

---
+++ src/main/java/rest/VerbosTest.java
@@ -0,0 +1,31 @@
+package rest;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+/**
+ * @author george on 03/09/2019
+ * @project trainingRestAssured
+ */
+public class VerbosTest {
+
+    @Test
+    public void deveSalvarUsuario() {
+        given()
+            .log().all()
+            .contentType("application/json")
+            .body("{ \"name\": \"jose\", \"age\": 50 }")
+        .when()
+            .post("https://restapi.wcaquino.me/users")
+        .then()
+            .log().all()
+            .statusCode(201)
+            .body("id", Matchers.is(Matchers.notNullValue()))
+            .body("name", Matchers.is("jose"))
+            .body("age", Matchers.is(50))
+        ;
+    }
+
+}
