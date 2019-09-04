f7b46267729ed5bd773f0f20665493cb416ac957
George Salu <george.salu10@gmail.com>
2019-09-03 22:48:37 -0300

    validação ao salvar

--- src/main/java/rest/VerbosTest.java
+++ src/main/java/rest/VerbosTest.java
@@ -28,4 +28,21 @@ public class VerbosTest {
         ;
     }
 
+    @Test
+    public void naoDeveSalvarUsuarioSemNome() {
+        given()
+            .log().all()
+            .contentType("application/json")
+            .body("{ \"age\": 50 }")
+        .when()
+            .post("https://restapi.wcaquino.me/users")
+        .then()
+            .log().all()
+            .statusCode(400)
+            .body("id", Matchers.is(Matchers.nullValue()))
+            .body("error", Matchers.is("Name é um atributo obrigatório"))
+        ;
+    }
+
+
 }
