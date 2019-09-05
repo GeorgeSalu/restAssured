5b121ad46605fea28fd2a43c4c10882ccd4e6af3
George Salu <george.salu10@gmail.com>
2019-09-04 21:35:54 -0300

    query

---
+++ src/main/java/rest/EnvioDadosTest.java
@@ -0,0 +1,27 @@
+package rest;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+/**
+ * @author george on 04/09/2019
+ * @project trainingRestAssured
+ */
+public class EnvioDadosTest {
+
+    @Test
+    public void deveEnviarValorViaQuery() {
+        given()
+            .log().all()
+        .when()
+            .get("https://restapi.wcaquino.me/v2/users?format=xml")
+        .then()
+            .log().all()
+            .statusCode(200)
+            .contentType(ContentType.XML)
+        ;
+    }
+
+}
