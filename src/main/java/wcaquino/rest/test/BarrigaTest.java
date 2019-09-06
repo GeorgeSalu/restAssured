8c61816f43c64d657a3237e79096bc540b1527d1
George Salu <george.salu10@gmail.com>
2019-09-05 23:38:31 -0300

    deve incluir conta com sucesso

--- src/main/java/wcaquino/rest/test/BarrigaTest.java
+++ src/main/java/wcaquino/rest/test/BarrigaTest.java
@@ -2,13 +2,17 @@ package wcaquino.rest.test;
 
 import org.junit.Test;
 import wcaquino.rest.core.BaseTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 /**
  * @author george on 05/09/2019
  * @project trainingRestAssured
  */
 public class BarrigaTest extends BaseTest {
-    
+
     @Test
     public void naoDeveAcessarAPISemToken() {
         given()
@@ -16,6 +20,31 @@ public class BarrigaTest extends BaseTest {
             .get("/contas")
         .then()
             .statusCode(401)
+        ;
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso() {
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "george.salu10@gmail.com");
+        login.put("senha", "saludasilva");
+
+        String token = given()
+            .body(login)
+        .when()
+            .post("/signin")
+        .then()
+            .statusCode(200)
+            .extract().path("token")
+        ;
+
+        given()
+            .header("Authorization", "JWT "+token)
+            .body("{ \"nome\": \"conta qualquer\"}")
+        .when()
+            .post("/contas")
+        .then()
+            .statusCode(201)
         ;
     }
 
